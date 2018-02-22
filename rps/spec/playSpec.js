36fb6c5f2b0cf3aa5e6c7f8cd2f4874811d8842a
mparker <mparker@pivotal.io>
2018-02-22 14:39:17 -0500

    refactored for ubiquitous language

--- rps/spec/playSpec.js
+++ rps/spec/playSpec.js
@@ -1,14 +1,14 @@
-function Rps(){
-    this.play = function(p1, p2, observer){
-        new PlayRequest(p1, p2, observer).process()
+function Requests(){
+    this.playRound = function(p1Throw, p2Throw, observer){
+        new PlayRoundRequest(p1Throw, p2Throw, observer).process()
     }
 }
 
-function PlayRequest(p1, p2, observer){
+function PlayRoundRequest(p1Throw, p2Throw, observer){
     this.process = function(){
-        if (inputInvalid())
+        if (throwInvalid())
             observer.invalid()
-        else if (draw())
+        else if (tie())
             observer.tie()
         else if (p1Wins())
             observer.p1Wins()
@@ -16,31 +16,30 @@ function PlayRequest(p1, p2, observer){
             observer.p2Wins()
     }
 
-    const validShapes = ["rock", "paper", "scissors"]
+    const validThrows = ["rock", "paper", "scissors"]
 
-    function inputInvalid() {
-        return !validShapes.includes(p1) || !validShapes.includes(p2)
+    function throwInvalid() {
+        return !validThrows.includes(p1Throw) || !validThrows.includes(p2Throw)
     }
 
-    function draw() {
-        return p1 === p2
+    function tie() {
+        return p1Throw === p2Throw
     }
 
     function p1Wins() {
         return (
-            p1 === "rock"     && p2 === "scissors" ||
-            p1 === "scissors" && p2 === "paper"    ||
-            p1 === "paper"    && p2 === "rock"
+            p1Throw === "rock"     && p2Throw === "scissors" ||
+            p1Throw === "scissors" && p2Throw === "paper"    ||
+            p1Throw === "paper"    && p2Throw === "rock"
         )
     }
-
-
 }
-describe("play", function () {
+
+describe("play Round", function () {
     let observerSpy, rps
 
     beforeEach(function () {
-        rps = new Rps()
+        rps = new Requests()
     })
 
     describe("p1 win scenarios", function () {
@@ -49,19 +48,19 @@ describe("play", function () {
         })
 
         it("rock v. scissors", function () {
-            rps.play("rock", "scissors", observerSpy)
+            rps.playRound("rock", "scissors", observerSpy)
 
             expect(observerSpy.p1Wins).toHaveBeenCalled()
         })
 
         it("scissors v. paper", function () {
-            rps.play("scissors", "paper", observerSpy)
+            rps.playRound("scissors", "paper", observerSpy)
 
             expect(observerSpy.p1Wins).toHaveBeenCalled()
         })
 
         it("paper v. rock", function () {
-            rps.play("paper", "rock", observerSpy)
+            rps.playRound("paper", "rock", observerSpy)
 
             expect(observerSpy.p1Wins).toHaveBeenCalled()
         })
@@ -75,19 +74,19 @@ describe("play", function () {
         })
 
         it("scissors v. rock", function () {
-            rps.play("scissors", "rock", observerSpy)
+            rps.playRound("scissors", "rock", observerSpy)
 
             expect(observerSpy.p2Wins).toHaveBeenCalled()
         })
 
         it("paper v. scissors", function () {
-            rps.play("paper", "scissors", observerSpy)
+            rps.playRound("paper", "scissors", observerSpy)
 
             expect(observerSpy.p2Wins).toHaveBeenCalled()
         })
 
         it("rock v. paper", function () {
-            rps.play("rock", "paper", observerSpy)
+            rps.playRound("rock", "paper", observerSpy)
 
             expect(observerSpy.p2Wins).toHaveBeenCalled()
         })
@@ -100,19 +99,19 @@ describe("play", function () {
         })
 
         it("rock v. rock", function () {
-            rps.play("rock", "rock", observerSpy)
+            rps.playRound("rock", "rock", observerSpy)
 
             expect(observerSpy.tie).toHaveBeenCalled()
         })
 
         it("paper v. paper", function () {
-            rps.play("paper", "paper", observerSpy)
+            rps.playRound("paper", "paper", observerSpy)
 
             expect(observerSpy.tie).toHaveBeenCalled()
         })
 
         it("scissors v. scissors", function () {
-            rps.play("scissors", "scissors", observerSpy)
+            rps.playRound("scissors", "scissors", observerSpy)
 
             expect(observerSpy.tie).toHaveBeenCalled()
         })
@@ -124,19 +123,19 @@ describe("play", function () {
         })
 
         it("rock v. <invalid>", function () {
-            rps.play("rock", Math.random(), observerSpy)
+            rps.playRound("rock", Math.random(), observerSpy)
 
             expect(observerSpy.invalid).toHaveBeenCalled()
         })
 
         it("<invalid> v. rock", function () {
-            rps.play(Math.random(), "rock", observerSpy)
+            rps.playRound(Math.random(), "rock", observerSpy)
 
             expect(observerSpy.invalid).toHaveBeenCalled()
         })
 
         it("invalid versus the same invalid", function () {
-            rps.play("sailboat", "sailboat", observerSpy)
+            rps.playRound("sailboat", "sailboat", observerSpy)
 
             expect(observerSpy.invalid).toHaveBeenCalled()
         })
