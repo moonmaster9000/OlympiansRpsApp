4bead18432ce39a02a910053a8643138cb653559
mparker <mparker@pivotal.io>
2018-02-23 12:26:51 -0500

    fake round repo test passing; contract test is still hardcoded

--- rps/spec/historySpec.js
+++ rps/spec/historySpec.js
@@ -11,7 +11,7 @@ describe("history", function () {
         })
     })
 
-    fdescribe("people have played", function () {
+    describe("people have played", function () {
         it("send the rounds to the observer", function () {
             let historyObserverSpy = jasmine.createSpyObj("historyObserverSpy", ["rounds"])
             let repo = {
@@ -28,5 +28,50 @@ describe("history", function () {
             ])
         })
 
+    })
+})
+
+function FakeRoundRepo(){
+    let rounds = []
+
+    this.isEmpty = function(){
+        return rounds.length === 0
+    }
+
+    this.save = function(r){
+        rounds.push(r)
+    }
+
+    this.getRounds = function(){
+        return rounds
+    }
+}
+
+fdescribe("repo contract", function () {
+    describe("when no rounds have been saved", function () {
+        it("is empty", function () {
+            expect(new FakeRoundRepo().isEmpty()).toBe(true)
+        })
+    })
+
+    describe("when rounds have been saved", function () {
+        it("is not empty", function () {
+            const repo = new FakeRoundRepo()
+
+            repo.save(new Round())
+
+            expect(repo.isEmpty()).toBe(false)
+        })
+
+        it("returns the rounds that have been saved", function () {
+            const repo = new FakeRoundRepo()
+            const round1 = new Round(Math.random())
+            const round2 = new Round()
+
+            repo.save(round1)
+            repo.save(round2)
+
+            expect(repo.getRounds()).toEqual([round1, round2])
+        })
     })
 })
