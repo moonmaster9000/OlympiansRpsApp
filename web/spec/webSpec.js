b2cdaf63744c0ae365b69490df107ce6e4de8dd4
mparker <mparker@pivotal.io>
2018-02-23 09:39:59 -0500

    first four frontend tests passing

--- web/spec/webSpec.js
+++ web/spec/webSpec.js
@@ -9,7 +9,23 @@ class PlayForm extends React.Component {
     }
 
     submitForm(){
+        this.props.requests.play("p1 throw placeholder", "p2 throw placeholder", this)
+    }
+    
+    tie(){
+        this.setState({outcome: "TIE"})
+    }
+
+    invalid(){
         this.setState({outcome: "INVALID"})
+    }
+
+    p1Wins(){
+        this.setState({outcome: "P1 Won!"})
+    }
+
+    p2Wins(){
+        this.setState({outcome: "P2 Won!"})
     }
 
     render(){
@@ -39,16 +55,73 @@ describe("play form", function () {
         })
     })
 
+    describe("the request processes as tie", function () {
+        beforeEach(function () {
+            let tieRequestStub = {
+                play(p1, p2, observer){
+                    observer.tie()
+                }
+            }
+
+            renderForm(tieRequestStub)
+        })
+
+        it("display 'TIE' to the user", function () {
+            expect(page()).not.toContain("TIE")
+            submitForm()
+            expect(page()).toContain("TIE")
+        })
+    })
+    
+    describe("the request processes as p1Wins", function () {
+        beforeEach(function () {
+            let p1WinsRequestStub = {
+                play(p1, p2, observer){
+                    observer.p1Wins()
+                }
+            }
+
+            renderForm(p1WinsRequestStub)
+        })
+
+        it("display 'P1Wins' to the user", function () {
+            expect(page()).not.toContain("P1 Won!")
+            submitForm()
+            expect(page()).toContain("P1 Won!")
+        })
+    })
+    
+    describe("the request processes as p2Wins", function () {
+        beforeEach(function () {
+            let p2WinsRequestStub = {
+                play(p1, p2, observer){
+                    observer.p2Wins()
+                }
+            }
+
+            renderForm(p2WinsRequestStub)
+        })
+
+        it("display 'P2Wins' to the user", function () {
+            expect(page()).not.toContain("P2 Won!")
+            submitForm()
+            expect(page()).toContain("P2 Won!")
+        })
+    })
+
     let domFixture
 
     function setupDOM() {
         domFixture = document.createElement("div")
-        domFixture.id = "hello!!!"
         document.body.appendChild(domFixture)
     }
 
     beforeEach(function () {
         setupDOM()
+    })
+
+    afterEach(function () {
+        domFixture.remove()
     })
 
     function renderForm(requests) {
